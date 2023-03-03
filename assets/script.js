857528b9564bafb7c584839046ed18e39e8c7f07
Nicholas DuFour <nick.dufour48@gmail.com>
2023-03-02 19:31:07 -0500

    added to html skeleton / added questions variable

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+var quizQuestions = [
+    {
+        question : "Question 1",
+        imageSrc : "",
+        answerChoiceA : "",
+        answerChoiceB : "",
+        answerChoiceC : "",
+        correctChoice : "B"
+    },{
+        question : "Question 2",
+        imageSrc : "",
+        answerChoiceA : "",
+        answerChoiceB : "",
+        answerChoiceC : "",
+        correctChoice : "A"
+    },{
+        question : "Question 3",
+        imageSrc : "",
+        answerChoiceA : "",
+        answerChoiceB : "",
+        answerChoiceC : "",
+        correctChoice : "D"
+    },{
+        question : "Question 4",
+        imageSrc : "",
+        answerChoiceA : "",
+        answerChoiceB : "",
+        answerChoiceC : "",
+        correctChoice : "C"
+    },
+];

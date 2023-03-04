802caedfada732ba6dad93056352c26dca466b7e
Nicholas DuFour <nick.dufour48@gmail.com>
2023-03-04 18:09:44 -0500

    worked on questions and answer function

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,53 @@
-
-
-// 0. OVERALL STRUCTURE: 
-// In index.html file, create array with 5 question objects. 
-// Store questions as an array of objects in a separate file named questions.js
-// Create code that will ask user questions, one after another. 
-// Multiple-Choice: Set up 3 choices for each and set a variable for correct answer.
-// Check user's answer choice against correct answer.
-// Provide user with an alert stating if it was correct or incorrect.
-// Keep track of the user score/progress.
-// When questions end or timer reaches 0, game ends
-// When game ends, add a form where user can save initials and store/display final score
-// Add buttons to go back/refresh and a button to clear high scores.
-
-// KEY STEPS:
 // 1. When user clicks the start button, then a timer starts and I am presented with a question
 
     // KEY VARIABLES: Declare variables 
     var startBtn = document.getElementById("startBtn");
     var countdownTimer = document.getElementById("countdownTimer");
     var homeContainer =  document.getElementById("homeContainer");
-    var quizContainer = document.querySelector("#quizContainer");
-    var quizQuestions = document.getElementById("quizQuestions");
-    var answerChoices = document.getElementById("answerChoices");
-    // var answerChoiceA = document.getElementById("A");
-    // var answerChoiceB = document.getElementById("B");
-    // var answerChoiceC = document.getElementById("B");
-    // var correctChoice = document.getElementById("correctChoice");
+    var quizContainer = document.getElementById("quizContainer");
+    var questionHeading = document.getElementById("questionHeading");
+    var answerChoice = document.getElementById("answerChoice");
+    var answerChoiceA = document.getElementById("answerChoiceA");
+    var answerChoiceB = document.getElementById("answerChoiceB");
+    var answerChoiceC = document.getElementById("answerChoiceC");
+    var answerChoiceD = document.getElementById("answerChoiceD");
+    // var correctAnswer = document.getElementById("correctAnswer");
     // var scoreDisplay = document.getElementById("userScore");
     // var finalTimeDisplay = document.getElementById("quizTimeFinal");
     // var quizResults = document.getElementById("#quizResults");
     // var submit = document.getElementById("#submit");
 
 // QUESTIONS ARRAY:
-// Store questions and choices as an array of objects in a separate file named questions.js
-
-// let questionOnePage = document.getElementById('question-1');
 
 var questionsArray = [
     {
         question: "Question 1: ?",
         imageSrc: "",
-        answerChoice: ["", "", "", ""],
-        answer: 1
+        answerChoice: ["a", "b", "c", "d"],
+        correctAnswer: 1
     }, 
     {
         question: "Question 2: ?",
         imageSrc: "",
-        answerChoice: ["", "", "", ""],
-        answer: 3
+        answerChoice: ["a", "b", "c", "d"],
+        correctAnswer: 3
     },
     {
         question: "Question 3: ?",
         imageSrc: "",
-        answerChoice: ["", "", "", ""],
-        answer: 3
+        answerChoice: ["a", "b", "c", "d"],
+        correctAnswer: 3
     }, 
     {
         question: "Question 4: ?",
         imageSrc: "",
-        answerChoice: ["", "", "", ""],
-        answer: 1
+        answerChoice: ["a", "b", "c", "d"],
+        correctAnswer: 1
     },
     {
         question: "Question 5: ?",
-        answerChoice: ["", "", "", ""],
-        answer: 0
+        answerChoice: ["a", "b", "c", "d"],
+        correctAnswer: 0
     }  
 ];
 
@@ -72,8 +55,8 @@ var questionsArray = [
     var score = 0;
     var secondsRemaining = 75;
     var countdownTimer = document.getElementById("countdownTimer");
-    var i = 0;
-    // var nextQuestions = 0;
+    var questionIndex = 0;
+    var nextQuestions = 0;
 
 // START EVENT LISTENER: Add event listeners for when user clicks Start button to start countdown timer and quiz. 
 
@@ -93,49 +76,48 @@ function setCountdownTimer() {
       }
     }, 1000);
   }
-    setCountdownTimer();
+
+// QUESTIONS FUNCTION: Set questions display function //
+function setQuizQuestions() {
+    for (let i = 0; i < questionsArray.length; i++) {
+        questionHeading.textContent = "Question: " + questionsArray[i].question; 
+        answerChoiceA.textContent = questionsArray[i].answerChoice[0]; 
+        answerChoiceB.textContent = questionsArray[i].answerChoice[1]; 
+        answerChoiceC.textContent = questionsArray[i].answerChoice[2]; 
+        answerChoiceD.textContent = questionsArray[i].answerChoice[3]; 
+    }
+}
 
 // START CODE QUIZ FUNCTION: Add start quiz function //
 function startCodeQuiz() {
-        homeContainer.style.display ="none";
-        quizContainer.style.display = "block";
-    }
-    // setQuizQuestions();
-    setCountdownTimer();
-    startCodeQuiz();
-    },
-
-// QUESTIONS FUNCTION: Set questions display function //
-// function setQuizQuestions() {
-//     for (let i = 0; i < answerChoices.length; i++) {
-//         questionHeading.textContent = answerChoices.question; 
-//         answerChoiceA.textContent = answerChoices[i].choices[0]; 
-//         answerChoiceB.textContent = answerChoices[i].choices[1]; 
-//         answerChoiceC.textContent = answerChoices[i].choices[2]; 
-//     }
-// }
+    homeContainer.style.display ="none";
+    quizContainer.style.display = "block";
+}
+setCountdownTimer();
+startCodeQuiz();
+setQuizQuestions();
+}),
 
 // 2. When user answers a question: then user is presented with another question
 // start quiz questions and display multiple-choice answers for user to choose from -> store answers
 // Add a function to direct the user to the next question using event delegation (event.target.value) and if else statements
 // add event listener to call this function to go to next question
 
-// Function to continue to the next question
-// function nextQuestions(event) {
-//     if (event.target.value === quizQuestions[nextQuestions].correctChoice) {
-//         score++;
-//         displayText.innerHTML = "Nailed it! Nice work!"
-//     } else {
-//         displayText.innerHTML = "Incorrect! Better luck next time!"
-//         secondsRemaining -= 3;
-//         countdownTimer.innerHTML= "Time: "+ secondsRemaining;
-//     }
-//     if (nextQuestions < quizQuestions.length -1) {
-//         nextQuestions++;
-//     } else {
-//         clearInterval(countdownTimerInterval);
-//     }
-//     setQuizQuestions();
+function nextQuestions(event) {
+    if (event.target.value === quizArray[nextQuestions].correctChoice) {
+        score++;
+        displayText.innerHTML = "Nailed it! Nice work!"
+    } else {
+        displayText.innerHTML = "Incorrect! Better luck next time!"
+        secondsRemaining -= 3;
+        countdownTimer.innerHTML= "Time: "+ secondsRemaining;
+    }
+    if (nextQuestions < quizArray.length -1) {
+        nextQuestions++;
+    } else {
+        clearInterval(countdownTimerInterval);
+    }
+    setQuizQuestions();
 
 // 3. When user answers a question correctly, increase the score
 // Use if, else statements
@@ -170,11 +152,14 @@ function startCodeQuiz() {
         function refreshQuiz(){
             window.location.reload();
             console.log(refreshQuiz);
-            },
+            }
+            refreshQuiz()
 
         // Use function with location.reload() and localStorage.clear() methods to clear high scores
         function clearQuiz(){
             window.location.reload();
             localStorage.clear();
             console.log(clearQuiz);
-            })
+            }
+
+        clearQuiz();
